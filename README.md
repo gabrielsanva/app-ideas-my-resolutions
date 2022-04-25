b722921a0a770a7bb2b1461f9353e6846a149ef3
gabrielsanva <gabrielsilvaiefap@gmail.com>
2022-04-24 23:54:57 -0300

    update readme

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Hello guys. This is a list of exercises that I am proposing to solve to develop 
 
 ### Beginner
 
-Project|Description|Challenge|My Resolution
-:-----:|:---------:|:-------:|:---:
-bin2dec|Binary-to-Decimal number converter|[Challenge](https://github.com/gabrielsanva/app-ideas/blob/master/Projects/1-Beginner/Bin2Dec-App.md)|[My Resolution](bin2dec/)
-real2centavos|Convert dollars to cents|[Chanllenge](https://github.com/gabrielsanva/app-ideas/blob/master/Projects/1-Beginner/Dollars-To-Cents-App.md)|[My Resolution](real2centavos)
+Project|Description|Original Challenge|Online
+:---:|:---:|:---:|:---:
+[bin2dec](bin2dec)|Binary-to-Decimal number converter|[Original Challenge](https://github.com/gabrielsanva/app-ideas/blob/master/Projects/1-Beginner/Bin2Dec-App.md)|[See Working]()
+[real2centavos](real2centavos)|Convert dollars to cents|[Original Challenge](https://github.com/gabrielsanva/app-ideas/blob/master/Projects/1-Beginner/Word-Frequency-App.md)|[See Working]()
---
+++ js/script.js
@@ -0,0 +1,45 @@
+const challenges = [
+    {
+        name: "bin2dec",
+        description: "Binary-to-Decimal number converter",
+        challenge: "1-Beginner/Bin2Dec-App.md"
+    },
+    {
+        name: "real2centavos",
+        description: "Convert dollars to cents",
+        challenge: "1-Beginner/Word-Frequency-App.md"
+    },
+]
+
+const table = document.getElementById("table");
+const createTable = () => {
+    let contentMD = "";
+    let content = "<tr>\n<th>Project</th>\n<th>Description</th>\n<th>Original Challenge</th>\n<th>GitHub</th>\n</tr>\n";
+    for (let count = 0; count < challenges.length; count++) {
+        content += "<tr>\n" +
+            "<td><a href='./" + challenges[count].name + "/'>" + challenges[count].name + "</a></td>\n" +
+            "<td>" + challenges[count].description + "</td>\n" +
+            "<td><a href='https://github.com/gabrielsanva/app-ideas/blob/master/Projects/" + challenges[count].challenge + "'>Challenge</a></td>\n" +
+            "<td><a href='https://github.com/gabrielsanva/app-ideas-my-resolutions/tree/main/" + challenges[count].name + "'>GitHub</a></td>\n" +
+            "</tr>";
+
+        contentMD += "[" + challenges[count].name + "](" + challenges[count].name + ")|" + challenges[count].description + "|" +
+            "[Original Challenge](https://github.com/gabrielsanva/app-ideas/blob/master/Projects/" + challenges[count].challenge + ")|" +
+            "[See Working]()\n";
+    }
+    table.innerHTML = content;
+    console.log(contentMD);
+}
+
+const links = document.getElementsByTagName("a");
+const linksBlank = () => {
+    for (let count = 0; count < links.length; count++) {
+        links[count].setAttribute("target", "_blank");
+    }
+}
+
+(function () {
+    createTable();
+    linksBlank();
+
+})()
